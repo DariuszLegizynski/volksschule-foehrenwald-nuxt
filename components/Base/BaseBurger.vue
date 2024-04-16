024e5f309fa6361778f94f2cb2f5d73b9e6394c9
Dariusz Legizynski <DLegizynski@anexia-it.com>
2024-04-16 22:23:31 +0200

    add header

---
+++ components/Base/BaseBurger.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+	const props = defineProps({
+		active: {
+			type: Boolean,
+			default: false,
+		},
+	})
+
+	const { active } = toRefs(props)
+</script>
+
+<template>
+	<div class="burger">
+		<div
+			class="burger-stick"
+			:class="{ active }"
+		/>
+	</div>
+</template>
+
+<style scoped>
+	.burger {
+		width: 3rem;
+		height: 3rem;
+		display: flex;
+		position: relative;
+		justify-content: center;
+		align-items: center;
+		cursor: pointer;
+	}
+
+	.burger-stick {
+		@apply w-8 h-[0.2rem] bg-white transition-all duration-500 ease-in-out rounded;
+	}
+
+	.burger-stick::before,
+	.burger-stick::after {
+		@apply content-[''] w-8 h-[0.2rem] absolute bg-white transition-all duration-500 ease-in-out rounded;
+	}
+
+	.burger-stick::before {
+		transform: translate(0px, -12px);
+	}
+
+	.burger-stick::after {
+		transform: translate(0px, 12px);
+	}
+
+	.active {
+		@apply bg-transparent;
+	}
+
+	.active::before {
+		@apply rotate-[45deg];
+	}
+
+	.active::after {
+		@apply rotate-[-45deg];
+	}
+</style>
+
---
+++ components/Sections/SectionsHeader.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+	const isBurgerActive = ref<boolean>(false)
+</script>
+
+<template>
+	<nav class="fixed top-0 left-0 right-0 bg-primary bg-opacity-50 z-10">
+		<ul class="flex justify-between items-center pl-2">
+			<img
+				src="@/assets/images/hero/logo.png"
+				alt="logo"
+				width="6rem"
+				height="2rem"
+				class="w-auto h-8"
+			/>
+			<BaseBurger
+				@click="isBurgerActive = !isBurgerActive"
+				:active="isBurgerActive"
+			/>
+
+			<!-- <li>
+				<BaseButton variant="transparent"><p>Über Uns</p></BaseButton>
+			</li>
+			<li>
+				<BaseButton variant="transparent"><p>Aktuelles</p></BaseButton>
+			</li>
+			<li>
+				<BaseButton variant="transparent"><p>Photos</p></BaseButton>
+			</li>
+			<li>
+				<BaseButton variant="transparent"><p>Header4</p></BaseButton>
+			</li>
+			<li>
+				<BaseButton variant="transparent"><p>Header5</p></BaseButton>
+			</li> -->
+		</ul>
+	</nav>
+</template>
+
