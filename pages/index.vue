2fe2e7ddbdb47cb65ebf7cf5872075e44898dd85
Dariusz Legizynski <DLegizynski@anexia-it.com>
2024-04-17 20:23:27 +0200

    add icons for nes page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
 	<Hero />
-	<div class="min-h-[500rem] relative z-1 bg-primary"></div>
+	<div class="min-h-[500rem] relative z-1 bg-primary">
+		<BaseIcons
+			type="swing"
+			fill="white"
+			width="4rem"
+			height="4rem"
+		/>
+	</div>
 </template>
 
