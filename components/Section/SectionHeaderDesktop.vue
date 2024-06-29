b62dd71c1c3711adbf36c620687b1f1ae83ef295
Dariusz Legizynski <d.legizynski@.com>
2024-06-29 19:27:36 +0200

    add desktop header and adjust font weight for navbar

---
+++ components/Section/SectionHeaderDesktop.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+	const isBurgerActive = ref<boolean>(false)
+
+	const hideNavbar = () => {
+		isBurgerActive.value = false
+	}
+</script>
+
+<template>
+	<nav class="fixed top-0 w-full bg-primary bg-opacity-50 z-10 py-2">
+		<section class="max-w-[80rem] mx-auto">
+			<section class="flex !justify-between items-center px-4">
+				<NuxtLink to="/">
+					<NuxtImg src="/images/hero/logo-1.png" alt="logo Volksschule Föhrenwald" width="6rem" height="2rem" class="w-auto h-10 pl-1" @click="hideNavbar" />
+				</NuxtLink>
+				<ul class="pl-4 flex items-center gap-x-4">
+					<li class="py-2">
+						<BaseButton link="/about" variant="navbar" class="text-white font-semibold" @click="hideNavbar"><p>Über Uns</p></BaseButton>
+					</li>
+					<li class="py-2">
+						<BaseButton variant="navbar" class="text-white font-semibold" @click="hideNavbar"><p>Aktuelles</p></BaseButton>
+					</li>
+					<li class="py-2">
+						<BaseButton variant="navbar" class="text-white font-semibold" @click="hideNavbar"><p>Photos</p></BaseButton>
+					</li>
+					<li class="py-2">
+						<BaseButton variant="navbar" class="text-white font-semibold" @click="hideNavbar"><p>Sidebar4</p></BaseButton>
+					</li>
+					<li class="py-2">
+						<BaseButton variant="navbar" class="text-white font-semibold" @click="hideNavbar"><p>Sidebar5</p></BaseButton>
+					</li>
+				</ul>
+			</section>
+		</section>
+	</nav>
+</template>
