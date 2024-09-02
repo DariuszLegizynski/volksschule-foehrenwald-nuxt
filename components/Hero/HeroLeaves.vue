25dd33cf2d688f21fae1dcdc43ddbd8116c0f426
Dariusz Legizynski <d.legizynski@.com>
2024-09-02 08:36:13 +0200

    adjust leaves

--- components/Hero/HeroLeaves.vue
+++ components/Hero/HeroLeaves.vue
@@ -103,9 +103,10 @@
 					y: -100,
 					scrollTrigger: {
 						trigger: leaf.value,
-						start: "top 300px",
+						start: "top 600px",
 						end: "top top",
 						scrub: 2,
+						markers: true,
 					},
 				})
 			})
@@ -125,8 +126,8 @@
 		<img ref="leaf8" class="top-[30%] right-[30%] leaf-style" src="/images/leaves/leave_8.png" />
 		<img ref="leaf9" class="top-[32%] left-[18%] leaf-style" src="/images/leaves/leave_9.png" />
 		<img ref="leaf10" class="top-[34%] right-[22%] leaf-style" src="/images/leaves/leave_10.png" />
-		<img ref="leaf11" class="top-[36%] left-[20%] leaf-style" src="/images/leaves/leave_1.png" />
-		<img ref="leaf12" class="top-[38%] right-[44%] leaf-style" src="/images/leaves/leave_2.png" />
+		<img ref="leaf11" class="top-[36%] left-[20%] leaf-style" src="/images/leaves/leave_11.png" />
+		<img ref="leaf12" class="top-[38%] right-[44%] leaf-style" src="/images/leaves/leave_12.png" />
 		<img ref="leaf13" class="top-[40%] left-[36%] leaf-style" src="/images/leaves/leave_3.png" />
 		<img ref="leaf14" class="top-[42%] right-[36%] leaf-style" src="/images/leaves/leave_4.png" />
 		<img ref="leaf15" class="top-[44%] left-[10%] leaf-style" src="/images/leaves/leave_5.png" />
@@ -135,8 +136,8 @@
 		<img ref="leaf18" class="top-[60%] left-[22%] leaf-style" src="/images/leaves/leave_8.png" />
 		<img ref="leaf19" class="top-[62%] right-[14%] leaf-style" src="/images/leaves/leave_9.png" />
 		<img ref="leaf20" class="top-[64%] left-[12%] leaf-style" src="/images/leaves/leave_10.png" />
-		<img ref="leaf21" class="top-[66%] right-[8%] leaf-style" src="/images/leaves/leave_1.png" />
-		<img ref="leaf22" class="top-[68%] left-[12%] leaf-style" src="/images/leaves/leave_2.png" />
+		<img ref="leaf21" class="top-[66%] right-[8%] leaf-style" src="/images/leaves/leave_11.png" />
+		<img ref="leaf22" class="top-[68%] left-[12%] leaf-style" src="/images/leaves/leave_12.png" />
 		<img ref="leaf23" class="top-[70%] right-[20%] leaf-style" src="/images/leaves/leave_3.png" />
 		<img ref="leaf24" class="top-[72%] left-[36%] leaf-style" src="/images/leaves/leave_4.png" />
 		<img ref="leaf25" class="top-[74%] right-[10%] leaf-style" src="/images/leaves/leave_5.png" />
