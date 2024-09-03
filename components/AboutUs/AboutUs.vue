0ef951e38adda4230a9cb0cb8f847692f24eeb50
Dariusz Legizynski <d.legizynski@.com>
2024-09-03 23:15:44 +0200

    add illustrations

--- components/AboutUs/AboutUs.vue
+++ components/AboutUs/AboutUs.vue
@@ -12,17 +12,18 @@
 	<article id="about-us" class="relative z-1 bg-white flex flex-col items-center text-primary py-16 px-4 gap-y-8">
 		<div class="centered-container">
 			<h2 class="pt-4 pb-12 text-center">{{ aboutUs?.title }}</h2>
-			<section class="grid md:grid-cols-2 lg:grid lg:grid-cols-3 lg:gap-x-8">
+			<section class="grid lg:grid lg:grid-cols-3 lg:gap-x-8">
 				<section class="pb-16" v-for="feature in aboutUs?.features" :key="feature.id">
-					<div class="flex flex-col items-center pb-12">
-						<AnimationFitness v-if="feature.animationType === 'animationfitness'" width="8rem" height="8rem" />
-						<AnimationSocial v-if="feature.animationType === 'animationsocial'" width="8rem" height="8rem" />
-						<BaseIcons v-if="feature.animationType === 'animationeco'" type="date" width="8rem" height="8rem" />
-						<BaseIcons v-if="feature.animationType === 'animationeco'" type="eko" width="8rem" height="8rem" />
+					<div class="grid grid-rows-[auto_auto_auto] gap items-center justify-items-center pb-12">
+						<AnimationPlaying v-if="feature.animationType === 'animation-playing'" width="16rem" height="16rem" />
+						<AnimationEco v-if="feature.animationType === 'animation-eco'" width="16rem" height="16rem" />
+						<AnimationLearning v-if="feature.animationType === 'animation-learning'" width="16rem" height="16rem" />
+						<AnimationFitness v-if="feature.animationType === 'animation-fitness'" width="8rem" height="8rem" />
+						<AnimationSocial v-if="feature.animationType === 'animation-social'" width="8rem" height="8rem" />
 						<div class="h3 text-center pb-2">{{ feature.title }}</div>
 						<AnimationUnderline height="36px" stroke="hsl(70, 70%, 46%)" fill="hsl(70, 70%, 46%)" stroke-width="8px" />
 					</div>
-					<p class="text-black text-center">{{ feature.description }}</p>
+					<p class="text-black text-center max-w-[40rem]">{{ feature.description }}</p>
 				</section>
 			</section>
 		</div>
