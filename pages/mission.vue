e2e47aba6f5a1b206bcfbb9931ad7c7f746837a6
Dariusz Legizynski <d.legizynski@.com>
2024-09-05 18:22:53 +0200

    add placeholders for future pages

---
+++ pages/mission.vue
@@ -0,0 +1,10 @@
+<template>
+	<article class="flex items-center justify-center min-h-dvh">
+		<div>
+			<h1 class="text-center text-3xl font-bold">Website im Aufbau</h1>
+			<div class="flex flex-col items-center pt-16 lg:mt-24 px-4">
+				<BaseButton link="/" variant="comic"><p>&larr; zurück</p></BaseButton>
+			</div>
+		</div>
+	</article>
+</template>
