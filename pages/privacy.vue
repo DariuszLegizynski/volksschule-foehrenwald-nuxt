9503395418051ff916d00f5cd4e08449329f83f0
Dariusz Legizynski <d.legizynski@.com>
2024-09-05 12:28:05 +0200

    add capitalize to privacy titel

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -12,7 +12,7 @@
 
 <template>
 	<article class="mt-24 px-4">
-		<h1 class="text-2xl pb-4">{{ privacyPolicyData?.title }}</h1>
+		<h1 class="text-2xl pb-4 capitalize">{{ privacyPolicyData?.title }}</h1>
 		<section v-html="privacyPolicyData?.content?.map(item => item.children.map(child => child.text).join('')).join('')"></section>
 		<BaseButton :link="privacyPolicyData?.link?.link" variant="navbar" class="!text-primary flex flex-col items-start pt-8">
 			<small>{{ privacyPolicyData?.link?.text }}</small>
