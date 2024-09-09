9c58d9975e885eeb55698cd3a2a733bb4361a4e5
Dariusz Legizynski <d.legizynski@.com>
2024-09-09 12:01:31 +0200

    add webp foto format

--- components/Photos/PhotosCard.vue
+++ components/Photos/PhotosCard.vue
@@ -13,7 +13,7 @@
 
 <template>
 	<NuxtLink :to="`/photos/${slug}`" class="relative w-full">
-		<NuxtImg loading="lazy" :src="imageSrc" :alt="imageAlt" width="6rem" height="8rem" class="w-full rounded-3xl h-[16rem] object-cover" />
+		<NuxtImg format="webp" loading="lazy" :src="imageSrc" :alt="imageAlt" width="6rem" height="8rem" class="w-full rounded-3xl h-[16rem] object-cover" />
 		<div class="absolute inset-0 bg-black opacity-20 rounded-3xl"></div>
 		<div class="absolute bottom-4 text-white w-full px-2 sm:flex sm:flex-col sm:justify-between sm:h-32 xl:h-40">
 			<div class="h3">{{ title }}</div>
