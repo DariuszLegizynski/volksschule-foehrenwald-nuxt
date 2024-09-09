c75e729f8988ebf476d611e36dba7b0251cad672
Dariusz Legizynski <d.legizynski@.com>
2024-09-09 11:21:16 +0200

    when possible show medium image format

--- pages/fotoGallery.vue
+++ pages/fotoGallery.vue
@@ -49,7 +49,9 @@
 					:title="fotoGallery.attributes?.title || 'No Title'"
 					:shortDescription="fotoGallery.attributes?.description || 'No Description'"
 					:imageSrc="
-						fotoGallery.attributes?.coverImage?.data?.attributes?.url
+						fotoGallery.attributes?.coverImage?.data?.attributes?.formats?.medium?.url
+							? `${strapiBaseUrl}${fotoGallery.attributes.coverImage.data.attributes.formats.medium.url}`
+							: fotoGallery.attributes?.coverImage?.data?.attributes?.url
 							? `${strapiBaseUrl}${fotoGallery.attributes.coverImage.data.attributes.url}`
 							: '/images/diverse/classmates-friends-bag-school-education.jpg'
 					"
@@ -62,3 +64,5 @@
 		</div>
 	</article>
 </template>
+
+<!-- // `${strapiBaseUrl}${fotoGallery.attributes?.coverImage?.data?.attributes?.url}` -->
